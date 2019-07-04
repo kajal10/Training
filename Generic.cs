c5c8de7f0f61c0fbbdb0136b038846fdb93016c2
kajal10 <52202768+kajal10@users.noreply.github.com>
2019-07-04 11:28:23 +0530

    Create Generic.cs

---
+++ Generic.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        public int add(int a, int b)
+        {
+            return(a + b);
+        }
+        public int mul(int a, int b)
+        {
+            return(a * b);
+        }
+        public int sub(int a, int b)
+        {
+            return(a - b);
+        }
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>();
+          
+            Program p = new Program();
+            lst.Add(p.add(10, 20));
+            lst.Add(p.sub(10, 20));
+            lst.Add(p.mul(10, 20));
+            foreach (int number in lst )
+                Console.WriteLine(number + "");
+            Console.ReadKey();
+        }
+    }
+}
